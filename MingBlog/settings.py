91161778f5e31d82737c8366a817b6dbdc6e3ca4
IHaoMing <31131565+IHaoMing@users.noreply.github.com>
2017-09-05 23:00:15 +0800

    部署修改

--- MingBlog/settings.py
+++ MingBlog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$#qjh+*d0z$p7k%ff=dz)q&u*j&6lqq1hnon@b6m-a%k9mqafp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.whaoming.site']
 
 
 # Application definition
@@ -122,3 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
